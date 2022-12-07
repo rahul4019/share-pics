6a05fe2851395bcb9d6e7b7e15cc2a4f054238fe
Rahul <rahulkashyap4019@gmail.com>
2022-12-07 08:11:27 +0530

    Install sanity.io and add required schemas for project

---
+++ sharepics_backend/schemas/save.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+      {
+        name: 'postedBy',
+        title: 'PostedBY',
+        type: 'postedBy',
+      },
+      {
+        name: 'userId',
+        title: 'UserId',
+        type: 'string',
+      },
+    ],
+  };
+  
