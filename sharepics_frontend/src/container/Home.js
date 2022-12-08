615f5b5d611918e4cb55344b00a0581396695b1f
Rahul <rahulkashyap4019@gmail.com>
2022-12-08 22:40:56 +0530

    Start working on Home page

--- sharepics_frontend/src/container/Home.js
+++ sharepics_frontend/src/container/Home.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../components';
+import Pins from './Pins';
+import { client } from '../client';
+import logo from '../assets/logo.png';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [toggleSidebar, setToggleSidebar] = useState(false);
 
-export default Home
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(false)}
+        />
+        <Link to='/'>
+          <img src={logo} alt="logo" className='w-28'/>
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={logo} alt="logo" className='w-28'/>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
