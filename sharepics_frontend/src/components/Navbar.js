99dca1a7fcc8fd4110cd3f55f3dc478e61bf28cd
Rahul <rahulkashyap4019@gmail.com>
2022-12-19 17:06:43 +0530

    Implement functionality of creating a new pin

--- sharepics_frontend/src/components/Navbar.js
+++ sharepics_frontend/src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
 
   return (
     <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 ">
-      <div className="flex justify-start items-start px-2 w-full rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+      <div className="flex  justify-start items-start px-2 w-full rounded-md bg-white border-none outline-none focus-within:shadow-sm">
         <IoMdSearch fontSize={21} className="ml-1" />
         <input
           type="text"
@@ -23,7 +23,7 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
       </div>
       <div className="flex gap-3 ">
         <Link to={`user-profile/${user?._id}`} className="hidden md:block">
-          <img src={user.image} alt="user" className="w-14 h-12 rounded-full" />
+          <img src={user?.user?.image} alt="user" className="w-14 h-12 rounded-full" />
         </Link>
         <Link
           to="/create-pin"
